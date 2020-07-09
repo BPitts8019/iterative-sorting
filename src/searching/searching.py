76852136797953105d8947cfb01008fd0cf66213
BPitts8019 <bradleyj.pitts@gmail.com>
2020-07-08 19:08:46 -0700

    Complete binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,18 +1,34 @@
 def linear_search(arr, target):
-    for elem in arr:
-        if elem is target:
-            return elem
+    for cur_index in range(len(arr)):
+        if arr[cur_index] == target:
+            return cur_index
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    is_done = False
+    start_index = 0
+    end_index = len(arr)-1
+    cur_index = (end_index - start_index) // 2
 
-    # Your code here
+    while len(arr) > 0 and not is_done:
+        if arr[cur_index] == target:
+            return cur_index
+
+        if cur_index >= end_index:
+            is_done = True
+        else:
+            if arr[cur_index] < target:
+                start_index = cur_index + 1
+            else:
+                end_index = cur_index - 1
+            cur_index = ((end_index - start_index) // 2) + start_index
 
     return -1  # not found
 
 
-nums = [6, 1, 5, 37, 55, 38, 12, 0, 98, 3, 4, 86, 8, 4, 66]
-print(linear_search(nums, 120))
+nums = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+nums2 = []
+print(binary_search(nums2, -6))
